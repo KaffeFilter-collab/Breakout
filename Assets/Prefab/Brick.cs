78d2b7cb8dd328bd86346e1651032ae5c40447b9
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2023-11-21 18:55:04 +0100

    last commit today

--- Assets/Prefab/Brick.cs
+++ Assets/Prefab/Brick.cs
@@ -1,45 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
+
 public class Brick : MonoBehaviour
 {
     private static int activeBricks=0;
-    public TextMeshProUGUI leveltxt;
     public int level = 1;
+    public delegate void BrickHitDelegate(Brick brickThatWasHit);
+    public static event BrickHitDelegate OnBrickHit;
+      public static event BrickHitDelegate NoBricksleft;
+
     private void Awake(){
 
     activeBricks++;
-}
+                        }
 
 
   private void  OnCollisionEnter2D(Collision2D collision) 
   {
    activeBricks--;
-    Destroy(gameObject);
+     gameObject.SetActive(false);
+      OnBrickHit.Invoke(this);
 
 
+         if(activeBricks =<)     
+            NoBricksleft.Invoke()this;
 
-    if(activeBricks==0){
-            level++;
-            leveltxt.text = level.ToString();
-  LoadNextScene();
+  
     }
   }
 
-  //TODO move somwhere else maby
-void LoadNextScene()
-{
-//Determin sceen index
- int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
- int targetsceneindex= currentSceneIndex+1;
-//check if next sceen index is out of bounds
-if (targetsceneindex >=SceneManager.sceneCountInBuildSettings)
-{
-targetsceneindex=0;
-}
-SceneManager.LoadScene(targetsceneindex);
-}
 
-}
--- Assets/Prefab/Gamemaster.cs
+++ Assets/Prefab/Gamemaster.cs
@@ -3,23 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 public class Gamemaster : MonoBehaviour
 {
-    
-    void Start()
-    {
-        
-    
-
-    }
-
-
-
-    public void Brickspawner(){
-
-
-
-    }
+   
 }
 
 
