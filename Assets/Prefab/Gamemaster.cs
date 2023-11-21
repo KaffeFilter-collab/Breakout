6ebb51bdcfed7aab69ba978f8c5e634fe19f6b2e
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2023-11-21 17:15:33 +0100

    gamemaster

---
+++ Assets/Prefab/Gamemaster.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class Gamemaster : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    
+
+    }
+
+
+
+    public void Brickspawner(){
+
+
+
+    }
+}
+
+
+ 
