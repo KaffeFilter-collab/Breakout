8e172744c8f67399112ca93c36767883dfe26db4
KaffeFilter-collab <148773655+KaffeFilter-collab@users.noreply.github.com>
2023-11-25 17:57:32 +0100

    New commit relevant

---
+++ Assets/Prefab/Brickplacer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+   public class Brickplacer : MonoBehaviour
+{
+
+    public Brick brickPrefab;
+    public int AbstandY = 05;
+    public int AbstandX = 10;
+    
+    
+    private void Awake()
+    {
+        GameObject.FindAnyObjectByType<Gamemaster>().Brickspawner += Brickspawner_on;
+    }
+
+
+
+    private void Brickspawner_on(Brick newBrick)
+    {
+        for (int  y= 0; y < 3; y++) { 
+        
+         
+            for (int i = 0; i < 3; i++)
+            {
+                Instantiate(brickPrefab, new Vector3(AbstandX*(i+3), AbstandY*(y-1)), Quaternion.identity);
+                
+            }
+            
+        }
+
+    }
+
+
+}
